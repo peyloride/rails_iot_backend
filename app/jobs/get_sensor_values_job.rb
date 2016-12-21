253d6003ff4a0b403779a3920b0cad5f022a6a89
Okan Binli <okanb3@gmail.com>
2016-12-21 14:35:38 +0300

    rails activejob added

---
+++ app/jobs/get_sensor_values_job.rb
@@ -0,0 +1,19 @@
+class GetSensorValuesJob < ApplicationJob
+  queue_as :default
+  after_perform do |job|
+    sleep 10
+    GetSensorValuesJob.perform_later('hebebebe')
+  end
+
+  def perform(*args)
+    # Do something later
+    User.all.each do |user|
+      user.sensors.each do |sensor|
+        sensor.values.create(light: CoAP::Client.new.get_by_uri("coap://#{sensor.address}/sensors/ambient_light").payload,
+                            temperature: CoAP::Client.new.get_by_uri("coap://#{sensor.address}/sensors/temperature").payload)
+        puts "#{sensor.address}"
+        puts CoAP::Client.new.get_by_uri("coap://#{sensor.address}/sensors/temperature").payload
+      end
+    end
+  end
+end
